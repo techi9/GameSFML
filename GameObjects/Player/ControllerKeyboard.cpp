bddc18655c0c93d025a7c2da404810160daea98e
techi9 <tucha.989@gmail.com>
2021-10-22 22:13:41 +0300

    Player control fix

--- GameObjects/Player/ControllerKeyboard.cpp
+++ GameObjects/Player/ControllerKeyboard.cpp
@@ -9,29 +9,22 @@ ControllerKeyboard::ControllerKeyboard(Player *player) {
     this->player = player;
 }
 
-void ControllerKeyboard::Control() { //TODO: fix moving diagonally
-    bool speedChanged = false;
+void ControllerKeyboard::Control() {
+
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-        speedX =- 1;
-        speedChanged = true;
+        speedX = - 1;
     }
+    else
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
         speedX = 1;
-        speedChanged = true;
     }
+    else {speedX = 0;}
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
         speedY = -1;
-        speedChanged = true;
-    }
+    }else
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
         speedY = 1;
-        speedChanged = true;
-    }
-    if(!speedChanged)
-    {
-        speedX = 0;
-        speedY = 0;
-    }
-    speedChanged = false;
+    }else {speedY = 0;}
+
     player->setSpeed(speedX, speedY);
 }
